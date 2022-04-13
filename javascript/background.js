6d0fe1c54e44835fdedc05d117b062ed4c8b8da9
pjl1234 <103551946+pjl1234@users.noreply.github.com>
2022-04-13 11:56:17 +0800

    校园一站通

---
+++ javascript/background.js
@@ -0,0 +1,28 @@
+function fugu(){
+    document.getElementById("background").style.backgroundColor="rgba(237,235,214)";
+    document.getElementById("fugu").style.color="rgba(89,142,221)";
+    document.getElementById("shensui").style.color="white";
+    document.getElementById("haitian").style.color="white";
+    document.getElementById("huyan").style.color="white";
+}
+function shensui(){
+    document.getElementById("background").style.backgroundColor="rgba(32,41,59)";
+    document.getElementById("shensui").style.color="rgba(89,142,221)";
+    document.getElementById("fugu").style.color="white";
+    document.getElementById("haitian").style.color="white";
+    document.getElementById("huyan").style.color="white";
+}
+function haitian(){
+    document.getElementById("background").style.backgroundColor="rgba(212,218,235)";
+    document.getElementById("haitian").style.color="rgba(89,142,221)";
+    document.getElementById("fugu").style.color="white";
+    document.getElementById("shensui").style.color="white";
+    document.getElementById("huyan").style.color="white";
+}
+function huyan(){
+    document.getElementById("background").style.backgroundColor="rgba(198,231,196)";
+    document.getElementById("huyan").style.color="rgba(89,142,221)";
+    document.getElementById("fugu").style.color="white";
+    document.getElementById("shensui").style.color="white";
+    document.getElementById("haitian").style.color="white";
+}
